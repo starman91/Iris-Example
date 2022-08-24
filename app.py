8c2fc3ef1c7438e948cf5ccebf3bf1e82e4db6e9
Robert White <Robert.White@nrel.gov>
2022-08-24 13:26:26 -0600

    Try relative path

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ from PIL import Image
 #def main():
 st.title("DuraMAT Streamlit Analysis App")
 
-dmt_image=Image.open('https://raw.githubusercontent.com/starman91/main/Iris-Example/images/logo-duramat-reversed-600.png ')
+dmt_image=Image.open('images/logo-duramat-reversed-600.png ')
 st.image(dmt_image)
 st.subheader("Hello Researcher")
     
